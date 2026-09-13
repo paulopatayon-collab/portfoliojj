6f671834fae20f28527d410db267137d5ca885c1
Paulo Patayon <paulopatayon@gmail.com>
2026-09-13 16:32:10 +0800

    Restore functional Canva portfolio runtime

--- scripts/site/media-protection.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('contextmenu', (e) => {
-        const isMedia = ['img', 'image', 'video', 'svg', 'picture'].some(
-          tagName => tagName.localeCompare(e.target.tagName, undefined, { sensitivity: 'base' }) === 0,
-        );
-        isMedia && e.preventDefault();
-      });
